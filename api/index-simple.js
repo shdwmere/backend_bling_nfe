f2ebb3d0207124527bffd062aad1d07c7a5592c6
unknown <caioszrm1@gmail.com>
2025-09-02 21:07:23 -0300

    changes

--- api/index-simple.js
+++ api/index-simple.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const blingRoutes = require('../routes/bling');
 
 const app = express();
+
+// CORS - permitir localhost para desenvolvimento
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? 
+  process.env.ALLOWED_ORIGINS.split(',') : 
+  ['http://localhost:5173', 'http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permite requests sem origin (mobile apps, Postman, etc.)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Não permitido pelo CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  optionsSuccessStatus: 200
+}));
 
 // Parser JSON básico
 app.use(express.json());
